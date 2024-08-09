4191863d2d8c3b6b7af1bf2916783e79cce05444
Daniel Schmitz <daniel@blendbyte.com>
2024-08-09 14:12:12 +0800

    wip

--- apps/icinga/src/main.cpp
+++ apps/icinga/src/main.cpp
@@ -3,31 +3,28 @@
 #include <kgfx.h>
 #include <HTTPClient.h>
 #include <ArduinoJSON.h>
+#include <Preferences.h>
 
-// CONFIGURATION
+Preferences preferences;
+OTAServer otaserver;
+KGFX ui;
+HTTPClient http;
+JsonDocument json;
+String icingaAPIURL;
+String icingaAPIUsername;
+String icingaAPIPassword;
+
+int refreshTimeInSeconds = 60;
+unsigned long lastTime = 0;
+bool authFailed = false;
+TFT_eSprite textSpr = ui.createSpriteLarge(240, 240);
+
+// CONFIGURATION:
 // Please make sure you enabled and configured the new API in your Icinga 2 installation by either setting it up manually
 // or use the CLI command for the automatic setup, which also creates a root-user for the API with a random password (icinga2 api setup)
 // The default port of the Icinga 2 API is 5665, so the URL to go with would be like: https://myicingaserver.com:5665
 // Requires the following API permission(s): status/query
 // Read more about Icinga 2 API Setup: https://icinga.com/docs/icinga-2/latest/doc/12-icinga2-api/
-preferences.begin("app", true);
-// String icingaAPIURL = "https://hostname:5665";
-// String icingaAPIUsername = "username";
-// String icingaAPIPassword = "password";
-icingaAPIURL = preferences.getString("icinga_url");
-icingaAPIUsername = preferences.getString("icinga_username");
-icingaAPIPassword = preferences.getString("icinga_password");
-preferences.end();
-int refreshTimeInSeconds = 60;
-// -- CONFIGURATION
-
-OTAServer otaserver;
-KGFX ui;
-unsigned long lastTime = 0;
-bool authFailed = false;
-TFT_eSprite textSpr = ui.createSpriteLarge(240, 240);
-HTTPClient http;
-JsonDocument json;
 
 void setup() {  
   Serial.begin(460800);
@@ -35,6 +32,15 @@ void setup() {
 
   otaserver.connectWiFi(); // DO NOT EDIT.
   otaserver.run(); // DO NOT EDIT
+
+  preferences.begin("app", true);
+  // icingaAPIURL = "https://hostname:5665";
+  // icingaAPIUsername = "username";
+  // icingaAPIPassword = "password";
+  String icingaAPIURL = preferences.getString("icinga_url");
+  String icingaAPIUsername = preferences.getString("icinga_username");
+  String icingaAPIPassword = preferences.getString("icinga_password");
+  preferences.end();
 
   ui.init();
   ui.clear();
